d802f691ae19ca4b31fef5c237394f5380ab6900
Liu, Yu <mark.liu@paypal.com>
2016-11-07 14:18:03 +0800

    up

--- one-module-with-unnamed-ok/compile-legacy.sh
+++ one-module-with-unnamed-ok/compile-legacy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x #echo on
-$JAVA9_BIN/javac -d classpath/de.codecentric.legacy.addresschecker/  -classpath modulepath/de.codecentric.zipvalidator/ $(find classpath -name "*.java")
+javac -d classpath/de.codecentric.legacy.addresschecker/  -classpath modulepath/de.codecentric.zipvalidator/ $(find classpath -name "*.java")
--- one-module-with-unnamed-ok/run-from-classpath.sh
+++ one-module-with-unnamed-ok/run-from-classpath.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -x #echo on
-$JAVA9_BIN/java -cp modulepath/de.codecentric.zipvalidator/:classpath/de.codecentric.legacy.addresschecker/ de.codecentric.legacy.addresschecker.api.Run $1
+java -cp modulepath/de.codecentric.zipvalidator/:classpath/de.codecentric.legacy.addresschecker/ \
+     de.codecentric.legacy.addresschecker.api.Run $1
